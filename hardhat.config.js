32ae8f28c8bfdc0ab2715f6ed8b8d3ac49a2e425
bellobambo <bellobambo21@gmail.com>
2024-09-05 05:00:34 +0100

    deployed contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-web3-v4"); // <================
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
 };
